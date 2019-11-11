3dd5ab8a3b31a8268e132f768c4d1c9b0deab3d8
MrSmithBA <57640957+MrSmithBA@users.noreply.github.com>
2019-11-11 22:28:18 +0100

    Nn
    Nn

--- command_draw.c
+++ command_draw.c
@@ -1,8 +1,8 @@
-/*
- * autor: Milos Kovac 
- * */
+
 #include "command_draw.h"
 #include "file_parser.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int color_matrix[480][640];
 
@@ -38,28 +38,20 @@ static void draw_cmd(int x1, int y1, int x2, int y2,color_id_t clr_id){
         default:
         break;
     }
-    if (y1=y2){//linija horizontalna
+    if (y1==y2){//linija horizontalna
         for (i =x1; i<x2; i++)
             color_matrix[y1][i]=color_to_write;
-        /*for (i=Y_MAX*y1+x1; i<Y_MAX*y1+x2;i++)
-            *(p_matrix+i)=color_to_write; //ovdje treba upisati boju */
-    }else if (x1=x2){//linija vertikalna
+    }else if (x1==x2){//linija vertikalna
         for (i =y1; i<y2; i++)
             color_matrix[i][x1]=color_to_write;
-            //color_matrix[y1][i]=color_to_write;
-        /*for (i=y1*Y_MAX+x1;i<y2*Y_MAX+x2;i+=Y_MAX) 
-            *(p_matrix+i)=color_to_write; // ovdje treba upisati boju*/
     }else {
         for (i=y1;i<y2;i++)
             for (k=x1;k<x2;k++)
                 color_matrix[i][k]=color_to_write;
-        /*for (i=y1;i<y2;i++)//rect
-            for (k=i*Y_MAX+x1;k<i*Y_MAX+x1+x2;k++)
-                *(p_matrix+k)=color_to_write;*/
     }
 }
         
-/*void copy_image_matrix(){
+void copy_image_matrix(){
     int x,y;
 	// If memory map is defined send image directly via mmap
 	int fd;
@@ -68,7 +60,7 @@ static void draw_cmd(int x1, int y1, int x2, int y2,color_id_t clr_id){
 	if (fd < 0)
 	{
 		printf("Cannot open /dev/vga for write\n");
-		return -1;
+		return ;
 	}
 	p=(int*)mmap(0,640*480*4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	memcpy(p, color_matrix, MAX_PKT_SIZE);
@@ -77,13 +69,13 @@ static void draw_cmd(int x1, int y1, int x2, int y2,color_id_t clr_id){
 	if (fd < 0)
 	{
 		printf("Cannot close /dev/vga for write\n");
-		return -1;
+		return ;
 	}
-} */
+} 
 
 void draw_all(command_buffer_t* p_cmd_buffer){
     int i=0;
     for (i=0;i<p_cmd_buffer->size;i++)
         draw_cmd(p_cmd_buffer->commands[i].x1,p_cmd_buffer->commands[i].y1,p_cmd_buffer->commands[i].x2,p_cmd_buffer->commands[i].y2,p_cmd_buffer->commands[i].clr_id);
-    //copy_image_matrix();
+    copy_image_matrix();
 }
--- command_draw.h
+++ command_draw.h
@@ -1,33 +1,23 @@
-/*
- * autor: Milos Kovac 
- * */
+
 #ifndef COMMAND_DRAW_H
 #define COMMAND_DRAW_H
-/*#include <unistd.h>
+#include <unistd.h>
 #include <sys/mman.h>
-#include <fcntl.h>*/
+#include <fcntl.h>
 #include "file_parser.h"
 
 #define MAX_PKT_SIZE (640*480*4)
 
-//comment to send pixels as commands via regular write function of char driver
-//leave uncommented to write directly to memory (faster)
-#define MMAP
 
 #define X_MAX 640
 #define Y_MAX 480
 #define SIZE_OF_IMAGE X_MAX*Y_MAX
-/*
- * U SLUCAJU STATICKE ALOKACIJE NEOPHODNO JE DEFINISATI MAKSIMALNU VELICINU LINIJE IZ FAJLA 
- * KAO I MAKSIMALAN BROJ LINIJA KOJE PROGRAM MOZE PROCESIRATI U FAJLU
- * ZASTITA NIJE REALIZOVANA U SLUCAJU DA SE OVAJ BROJ PREMASI
- * U TOM SLUCAJU NASTUPA PISANJE PO NULL POINTERU ILI JOS GORE, PISANJE PO NAREDNOM ALOCIRANOM PROSTORU STO MOZE DOVESTI DO NEKONZISTENTNOSTI PODATAKA ! 
- * */
 
-#define RGB_RED     0x1F00
+
+#define RGB_RED     0xF800
 #define RGB_BLACK   0x0000
 #define RGB_WHITE   0xFFFF
-#define RGB_GREEN   0x03F0
+#define RGB_GREEN   0x07E0
 #define RGB_YELLOW  (RGB_RED | RGB_GREEN)
 #define RGB_BLUE    0x001F
 
